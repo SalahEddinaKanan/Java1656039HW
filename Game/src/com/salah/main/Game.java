0730cc4aa991cf80bf357ab83a861de43ba7607f
Salah Eddin Kanan <52652328+SalahEddinaKanan@users.noreply.github.com>
2020-12-19 11:48:02 +0200

    Update Game.java

--- Game/src/com/salah/main/Game.java
+++ Game/src/com/salah/main/Game.java
@@ -30,13 +30,45 @@ public class Game extends Canvas implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
-	public void run() {
+	// Not mine
+	//  Notch  the game developer of minecraft uses it
+	public void run() {  
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				
+			}
+		}
+		stop();
+	}
+	private void tick() {
+		
+	}
+	private void render() {
 		
 	}
 
 	public static void main(String arg[]) {
-		
+		new Game();
 	}
 
 }
