d0314d00df983dad29d55db54a9e3263e5f469cf
Salah Eddin Kanan <52652328+SalahEddinaKanan@users.noreply.github.com>
2020-12-21 18:10:11 +0200

    Add files via upload

---
+++ Game/src/Game/ID.java
@@ -0,0 +1,16 @@
+package Game;
+
+import Game.Game;
+
+
+public enum ID {
+
+    Player(),
+    BasicEnemy(),
+    FastEnemy(),
+    SmartEnemy(),
+    BossEnemy(),
+    MenuParticle(),
+    Trail(),
+    PlayerBullets()
+}
---
+++ Game/src/Game/Player.java
@@ -0,0 +1,61 @@
+package Game;
+
+import java.awt.*;
+
+
+public class Player extends GameObject {
+
+    //Random r = new Random();
+    Handler handler = new Handler();
+
+    public Player(int x, int y, ID id, Handler handler) {
+        super(x, y, id);
+        this.handler = handler;
+
+
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle((int)x, (int)y, 32, 32);
+    }
+
+    public void tick() {
+        x += velX;
+        y += velY;
+
+        x = (int) Game.clamp(x, 0, Game.WIDTH - 38);
+        y = (int) Game.clamp(y, 0, Game.HEIGHT - 61);
+
+        collision();
+    }
+
+    private void collision() { //kolizja gracza z czymœ innym
+        for (int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i); //zbiera wszystkie objecty z gry
+
+            if (tempObject.getId() == ID.BasicEnemy || tempObject.getId() == ID.FastEnemy || tempObject.getId() == ID.SmartEnemy) { //kolizja z BasicEnemy, czyli tempObject jest naszym BasicEnemy
+                if (getBounds().intersects(tempObject.getBounds())) {
+                    //collision code
+                    HUD.HEALTH -= 2;
+                }
+            }
+            if (tempObject.getId() == ID.BossEnemy) { //kolizja z BasicEnemy, czyli tempObject jest naszym BasicEnemy
+                if (getBounds().intersects(tempObject.getBounds())) {
+                    //collision code
+                    HUD.HEALTH -= 100;
+                }
+            }
+
+        }
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.gray);
+        g.fillRect((int)x, (int)y, 32, 32);
+        g.setColor(Color.red);
+        g.drawRect((int)x , (int)y, 32, 32);
+    }
+
+
+
+}
